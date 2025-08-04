a01f6edc49b16b5cadc78157736f75bf58c78480
Didin <didinpg8080@gmail.com>
2025-08-04 06:09:29 +0000

    test

--- classmanager/settings.py
+++ classmanager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '13x8r2se41dawv$7!a-p_h4ni$^i^+r@le7wuadd6gmzb=$ss*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.169.38.193']
+ALLOWED_HOSTS = ['35.169.38.193','a7d2f5480726a45148921a809870da87-275944914.eu-north-1.elb.amazonaws.com']
 
 AUTH_USER_MODEL = 'classroom.User'
 
